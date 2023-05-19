e17250bf4c6ab6fca31a3b7162cfff3e1869e627
BidhanCR <bidhanroy300805@gmail.com>
2023-05-19 20:55:24 +0600

    seach query added

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const AllToys = () => {
   const [toys, setToys] = useState([]);
   const [isAllLoaded, setIsAllLoaded] = useState(false);
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
     fetch(`http://localhost:5000/toys?limit=20`)
       .then((res) => res.json())
@@ -12,6 +13,7 @@ const AllToys = () => {
         setIsAllLoaded(false);
       });
   }, []);
+  
   const handleSeeAll = () => {
     fetch(`http://localhost:5000/toys`)
       .then((res) => res.json())
@@ -20,8 +22,45 @@ const AllToys = () => {
         setIsAllLoaded(true);
       });
   };
+
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/toys/search/${searchText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setToys(data);
+      });
+  };
   return (
     <div>
+      <div className="flex justify-center my-6">
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              onChange={(e) => setSearchText(e.target.value)}
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="overflow-x-auto">
         <table className="table w-full">
           <thead>
@@ -53,11 +92,11 @@ const AllToys = () => {
         </table>
       </div>
 
-      
       {!isAllLoaded && (
-        <button onClick={handleSeeAll} className="btn btn-primary">See All</button>
+        <button onClick={handleSeeAll} className="btn btn-primary">
+          See All
+        </button>
       )}
-  
     </div>
   );
 };
