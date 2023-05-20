af8ba29456ceec7a7ab049fce6420c4c64151259
BidhanCR <bidhanroy300805@gmail.com>
2023-05-20 14:19:54 +0600

    toys gallery updated

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,27 +1,33 @@
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Gallery = () => {
-    const carImages = [
-        'https://media.istockphoto.com/id/1336889958/photo/classic-model-toy-car-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=D8rwhv8-ntojZLGPARPlhwlR7qtMNxOjTySntBrayo0=',
-        'https://media.istockphoto.com/id/121666842/photo/blue-toy-car-trabant-isolated-on-white.jpg?s=612x612&w=is&k=20&c=svcZQRNsMjLiQUQlehdfqXJjvybCT7VNXMVgEavnmrk=',
-        'https://media.istockphoto.com/id/680079488/photo/retro-car-orange-1960.jpg?b=1&s=170667a&w=0&k=20&c=XRq_2v2IXp6AONLJxddQtgvfh15mGNqgSZ3XyvjO_u4=',
-        'https://media.istockphoto.com/id/1336889958/photo/classic-model-toy-car-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=D8rwhv8-ntojZLGPARPlhwlR7qtMNxOjTySntBrayo0=',
-        'https://media.istockphoto.com/id/121666842/photo/blue-toy-car-trabant-isolated-on-white.jpg?s=612x612&w=is&k=20&c=svcZQRNsMjLiQUQlehdfqXJjvybCT7VNXMVgEavnmrk=',
-        'https://media.istockphoto.com/id/680079488/photo/retro-car-orange-1960.jpg?b=1&s=170667a&w=0&k=20&c=XRq_2v2IXp6AONLJxddQtgvfh15mGNqgSZ3XyvjO_u4=',
-        // Add more image URLs here
-      ];
-    return (
-        <section className="text-center mt-8">
-      <h2 className="text-3xl font-bold mb-4">Toy Car Gallery</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {carImages.map((image, index) => (
-          <div className="rounded overflow-hidden" key={index}>
-            <img src={image} alt={`Car ${index + 1}`} className="w-full border-2  h-[300px] transform hover:scale-105 transition-transform duration-300" />
+  const [toys, setToys] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/toys")
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
+  return (
+    <section className="text-center mt-8">
+      <div className="flex">
+        {toys.map((toy, index) => (
+          <div className="rounded overflow-hidden relative" key={index}>
+            <img
+              src={toy.picture}
+              alt={toy.name}
+              className="w-full border-2  h-[300px] transform hover:scale-105 transition-transform duration-300"
+            />
+            <div className="absolute bottom-0 right-0">
+              <Link to={`/toy/${toy._id}`}>
+                <button className="btn btn-outline btn-success">Details</button>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
     </section>
-    );
+  );
 };
 
 export default Gallery;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Gallery from "../Gallery/Gallery";
 import Banner from "./Banner";
-
+import Marquee from "react-fast-marquee";
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <Gallery></Gallery>
+      <h3 className="text-3xl font-bold mt-12 text-center">Toys Gallery</h3>
+      <Marquee direction="left" speed={40} pauseOnHover={true}>
+        <Gallery></Gallery>
+      </Marquee>
+ 
     </div>
   );
 };
